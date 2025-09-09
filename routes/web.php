1c08dd4a2047cd41359c1b3df0c1b409b1b79f12
frostoy <123365686+Frostoy@users.noreply.github.com>
2025-09-10 01:24:22 +0700

    Feature/dashboard (#3)
    * feat: implement dashboard page
    
    * implement dashboard frontpage
    
    ---------
    
    Co-authored-by: Geraydodo <geraydojessen1@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
@@ -8,3 +9,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
