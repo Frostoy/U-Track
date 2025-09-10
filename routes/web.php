1aabf5bbd9c3d1f4e621c23891fe12caf0280aea
frostoy <123365686+Frostoy@users.noreply.github.com>
2025-09-10 11:22:25 +0700

    feat: finish inventory frontend (#4)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
@@ -11,3 +12,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory');
