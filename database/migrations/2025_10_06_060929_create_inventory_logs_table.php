e89b494a6f245f0199526e86fcdd774525821603
frostoy <123365686+Frostoy@users.noreply.github.com>
2025-10-06 13:41:47 +0700

    Initial database integration with migrations, models, seeders (#6)

---
+++ database/migrations/2025_10_06_060929_create_inventory_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('medicine_id');
+            $table->unsignedBigInteger('user_id');
+            $table->enum('type', ['in', 'out']);
+            $table->integer('quantity');
+            $table->text('note')->nullable();
+            $table->timestamps();
+
+            $table->foreign('medicine_id')->references('id')->on('medicines')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_logs');
+    }
+};
