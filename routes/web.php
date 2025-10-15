ec11e20bbade60e7aa1dabd54c94b8d1f28c45d1
frostoy <123365686+Frostoy@users.noreply.github.com>
2025-10-15 08:46:11 +0700

    feat(auth): develop frontend and implement middleware for routes (#8)
    Co-authored-by: Geraydodo <geraydojessen1@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory');
     Route::get('/reports', [ReportsController::class, 'index'])->name('reports');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 });
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
